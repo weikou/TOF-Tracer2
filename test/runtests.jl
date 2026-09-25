85d574b0fa6982a4ac4d628b5b62a78d9bf53830
Wiebke <wwiewiebke@gmx.de>
2026-09-25 09:42:58 +0200

    fixed small issue in InterpolationFunctions

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,15 +8,25 @@ using Suppressor
 #DocMeta.setdocmeta!(TOFTracer2, :DocTestSetup, :(using TOFTracer2); recursive=true)
 
 @testset "allTests" begin
+	println("testing processingWorkflowAPI...")
 	include("./test_processingWorkflowAPi.jl")
+	println("testing processingWorkflow...")
 	include("./test_processingWorkflow.jl")
+	println("testing TOFFunctions...")
 	include("./test_includes/test_TOFFunctions.jl")
+	println("testing CalibrationFunctions...")
 	include("./test_includes/test_CalibrationFunctions.jl")
+	println("testing InterpolationFunctions...")
 	include("./test_includes/test_InterpolationFunctions.jl")
+	println("testing ResultFileFunctions...")
 	include("./test_includes/test_ResultFileFunctions.jl")
+	println("testing MasslistFunctions...")
 	include("./test_includes/test_MasslistFunctions.jl")
+	println("testing ExportFunctions...")
 	include("./test_includes/test_ExportFunctions.jl")
+	println("testing ImportFunctions...")
 	include("./test_includes/test_ImportFunctions.jl")
+	println("testing PlotFunctions...")
 	include("./test_includes/test_PlotFunctions.jl")
 end
 
